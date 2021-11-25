541509f6fbc613d8960ea2981bc3690f9acee3ac
serdim <sergio.dimartino@unina.it>
2021-11-25 18:09:30 +0100

    modificato saluto

--- Prova.java
+++ Prova.java
@@ -1,6 +1,6 @@
 public class Prova{
 	public static void main(String args[]){
-		System.out.println("Hello Sergio");
+		System.out.println("Hello Programmazione a oggettti");
 		
 		System.out.println("Hello Luigi!");
 		
